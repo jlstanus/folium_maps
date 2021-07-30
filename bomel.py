c7586b7c7061d013ac513922b0433ec6b6e015f1
jl.stanus <rammstan@msn.com>
2021-07-30 16:22:26 +0200

    create bomel map

---
+++ bomel.py
@@ -0,0 +1,11 @@
+import os
+import folium
+
+m = folium.Map(location=[50.4739, 4.8587], tiles="Stamen Terrain", zoom_start=13)
+
+folium.Marker([50.47433, 4.86478], popup="Hello World").add_to(m)
+
+m.add_child(folium.ClickForMarker(popup="Waypoint"))
+
+
+m.save(os.path.join('html_maps', 'bomel.html'))

b4d0837d29b1929444725a886175c9ba26ced1d1
jl.stanus <rammstan@msn.com>
2021-07-30 17:15:37 +0200

    bomel map: increase zoom

--- bomel.py
+++ bomel.py
@@ -1,7 +1,7 @@
 import os
 import folium
 
-m = folium.Map(location=[50.4739, 4.8587], tiles="Stamen Terrain", zoom_start=13)
+m = folium.Map(location=[50.4739, 4.8587], tiles="Stamen Terrain", zoom_start=15)
 
 folium.Marker([50.47433, 4.86478], popup="Hello World").add_to(m)
 
